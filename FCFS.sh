2674e63ea0cd9d0eb00e0c73046c5986932b467d
Mostafa-Rawash <mostafa@rawash.com>
2021-06-02 19:59:03 +0200

    adding some comments

--- FCFS.sh
+++ FCFS.sh
@@ -1,21 +1,18 @@
-exit=1
+stay="yes"
 echo "Welcome to FCFS implementation"
 
 
-
-#  functions init. 
-function takeData () {
-echo "Enter the number  of processes that you want"
-read f
+while [ $stay == "yes" ];do #  to run the program many times untel the user end thier processes
+echo "Enter the number of processes that you want to calculate" #  take the the numer of process from the user 
+read f 
+# store the num. of processes in var. "f"
 waitingTime=0
-avarage=0
-for ((x=1;x<=$f;x++))
+# waiting time for first processes will be 0 by default
+avarage=0 # will collect each process brust time then divided them by  
+for ((x=1;x<=$f;x++)) # looping to take each processes brust time 
 do
-# echo "Enter the duration time for process num.$x  "
-# read durationTime
-echo "Enter the burst time for process num.$x"
+echo "Enter the burst/duration time for process num.$x" # to enter the brust time for the process num. $x
 read burstTime
-
 if [ $x != $f  ]
 then
 waitingTime=$((waitingTime+burstTime))
@@ -24,22 +21,9 @@ waitingTime=0
 fi
 echo "waitingTime is $waitingTime"
 totalBrutTime=$(($totalBrutTime+$waitingTime))
-echo "totalBrutTime is until now is $totalBrutTime "
-done
-avarage=$((totalBrutTime/f))
-printf "avarage time to nerest integer is equal: $avarage"
-}
-takeData
-
-# #  to run the program many times
-# while (($exit!=1))
-# do
-# 
-# done
-
-
-
-
-
-# Notes
-# to adding variable in string using $ before var. name
+done # end of for loop
+avarage=$((totalBrutTime/f))  # will devide the total of brusting time by num. of processes 
+echo "avarage time to nerest integer is equal: $avarage "
+echo "did you want to make new process? (yes or no)"
+read stay # if the user enter "no" the program will stop 
+done # end of while loop
